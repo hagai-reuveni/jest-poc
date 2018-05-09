ac4d486827cac6a65b743d5ce70576c7d535fde4
Hagai Reuveni <reuveni.hagai@oath.com>
2018-05-09 13:28:09 +0300

    Adding react-redux sample project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+// react deps
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// redux deps
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import reducer from './reducers';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = [];
+if (process.env.NODE_ENV !== 'production') middleware.push(logger);
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware),
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
