3b5e0d613aabadc26201d4fc51ae37d67bf59d7e
Hagai Reuveni <reuveni.hagai@oath.com>
2018-05-09 15:14:50 +0300

    Adding app spec

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
 //console.log(window); // this is to test if jest is running with js-dom
