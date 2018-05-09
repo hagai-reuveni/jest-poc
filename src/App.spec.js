d34fa9ba54e9460b76975ef3bcf881df26bdb743
Hagai Reuveni <reuveni.hagai@oath.com>
2018-05-09 15:33:15 +0300

    Adding test

--- src/App.spec.js
+++ src/App.spec.js
@@ -7,5 +7,10 @@ describe('<App />', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should render an `h1', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('h1').text()).toEqual('This is a jest poc test');
+  });
 });
 //console.log(window); // this is to test if jest is running with js-dom
